b85c310d7166b821758eebd3dc93833a63d2e403
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2025-02-19 10:28:03 +0800

    Updating the dependencies for accessing google sheets

--- src/main/java/com/personal/budget/clients/google/SheetsServiceConfig.java
+++ src/main/java/com/personal/budget/clients/google/SheetsServiceConfig.java
@@ -3,7 +3,7 @@ package com.personal.budget.clients.google;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,10 +23,7 @@ public class SheetsServiceConfig {
 
     @Bean
     public Sheets sheets(Credential credential) throws IOException, GeneralSecurityException {
-        return new Sheets.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(),
-                credential)
+        return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(), GsonFactory.getDefaultInstance(), credential)
                 .setApplicationName(APPLICATION_NAME)
                 .build();
     }
