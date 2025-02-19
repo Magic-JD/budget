aa8a4302c10a23707cd68b6695d9ba5664ec87c6
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2025-02-19 12:58:18 +0800

    Native experiementation tests

--- src/main/java/com/personal/budget/clients/google/SheetClient.java
+++ src/main/java/com/personal/budget/clients/google/SheetClient.java
@@ -1,38 +1,35 @@
 package com.personal.budget.clients.google;
 
-import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.model.ValueRange;
 import com.personal.budget.data.BudgetLine;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.time.ZonedDateTime;
-import java.util.List;
 
 @Component
 public class SheetClient {
 
-    private final Sheets sheets;
-    private final String sheetId;
-
-    public SheetClient(Sheets sheets, @Value("${application.sheet-id}") String sheetId) {
-        this.sheets = sheets;
-        this.sheetId = sheetId;
-    }
-
+//    private final Sheets sheets;
+//    private final String sheetId;
+//
+//    public SheetClient(Sheets sheets, @Value("${application.sheet-id}") String sheetId) {
+//        this.sheets = sheets;
+//        this.sheetId = sheetId;
+//    }
+//
     public boolean updateSheet(BudgetLine budgetLine, ZonedDateTime currentTime) {
-        ValueRange body = new ValueRange()
-                .setValues(List.of(List.of(budgetLine.type().name(), budgetLine.money().amount(), budgetLine.comment(), currentTime.toLocalDate().toString())));
-        try {
-            sheets.spreadsheets().values()
-                    .append(sheetId, "A1", body)
-                    .setValueInputOption("RAW")
-                    .execute();
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
+//        ValueRange body = new ValueRange()
+//                .setValues(List.of(List.of(budgetLine.type().name(), budgetLine.money().amount(), budgetLine.comment(), currentTime.toLocalDate().toString())));
+//        try {
+//            sheets.spreadsheets().values()
+//                    .append(sheetId, "A1", body)
+//                    .setValueInputOption("RAW")
+//                    .execute();
+//            return true;
+//        } catch (IOException e) {
+//            return false;
+//        }
+//    }
+        return true;
     }
 
 }
